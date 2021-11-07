c57c8b929dc3b03a93afdf395978da3f9e392624
FSauna <yfuta0630@gmail.com>
2021-11-07 06:25:30 +0000

    [Add] 投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,49 @@
 class PostsController < ApplicationController
   
   def new
+    @post = Post.new
   end
   
   def create
   end
   
   def index
+    @posts = Post.all
+    @user = current_user
   end
   
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
   
   def edit
+    @post = Post.find(params[:id])
+    if @post.user == current_user
+        render "edit"
+    else
+        redirect_to posts_path
+    end
   end
   
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id), flash: { notice: 'You have updated book successfully.' }
+    else
+      render :edit
+    end
   end
   
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:body, :image_id)
   end
   
 end
