83a2927e0884f1f282d907c4d7a83eb8d95e7972
FSauna <yfuta0630@gmail.com>
2021-12-18 11:18:38 +0000

    [Add] likeモデルテスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションのテスト' do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context 'Userモデルとの関係' do
+      let(:target) { :user }
+
+      it 'N:1となっている' do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+
+    context 'Micropostモデルとの関係' do
+      let(:target) { :micropost }
+
+      it 'N:1となっている' do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+  end
 end
