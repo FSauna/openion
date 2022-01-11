84cb3cc996d216c14a6549a0308606acecd789ed
FSauna <yfuta0630@gmail.com>
2022-01-11 12:59:34 +0000

    [Fix] タイムゾーン修正

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module Openion
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
 
-config.time_zone = 'Tokyo'
